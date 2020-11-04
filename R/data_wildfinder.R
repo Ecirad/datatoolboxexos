f85313f5684071b04bb1978d7287351409a4b8bf
Eliott Cirad <eliott.maurent@edu.mnhn.fr>
2020-11-04 09:28:36 +0100

    Création des fonctions et packages utilisés

---
+++ R/data_wildfinder.R
@@ -0,0 +1,31 @@
+#' Get ecoregion data
+#'
+#' @return data.frame with info on ecoregions
+#' @export
+#'
+#' @examples
+data_ecoregion = function(){
+  readr::read_csv(here::here("data", "wwf_wildfinder", "wildfinder-ecoregions_list.csv"))
+}
+
+
+#' Get mammal species data
+#'
+#' @return data.frame with info on mammal
+#' @export
+#'
+#' @examples
+data_mammals = function(){
+  readr::read_csv(here::here("data", "wwf_wildfinder", "wildfinder-mammals_list.csv"))
+}
+
+
+#' Get mammal <-> ecoregion data
+#'
+#' @return data.frame on which mammals in ecoregions
+#' @export
+#'
+#' @examples
+data_mammals_ecoregion = function(){
+  readr::read_csv(here::here("data", "wwf_wildfinder", "wildfinder-ecoregions_species.csv"))
+}
